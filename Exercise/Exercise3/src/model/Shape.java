9046018ef7983647b2d8a69219bf2dfc4439663d
YUNJIA SUN <98619973+YunjiaSun@users.noreply.github.com>
2022-11-18 22:53:44 -0800

    Delete Exercise/Exercise3 directory

--- Exercise/Exercise3/src/model/Shape.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public abstract class Shape implements java.io.Serializable {
-    public abstract double calculateArea();
-    public abstract double calculatePerimeter();
-    public abstract void printInfo();
-}
